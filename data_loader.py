db4f5bd9d9a3cd2359946512dcdc44bdd5c5521f
rachelzeng98 <qiuruizeng98@gmail.com>
2024-12-08 08:01:01 +0000

    make multi-modality more general

--- data_loader.py
+++ data_loader.py
@@ -1,49 +1,44 @@
 import pandas as pd
 import numpy as np
+from concurrent.futures import ThreadPoolExecutor
+
 
 # import wandb
 # wandb.init(project='gene')
 
 #ct = 'DG_Glut'
 
-def load_data(ct):
-    df = pd.read_csv(f'ml_input/{ct}/{ct}.luisa_RNA_DEG.csv', index_col =0)
-    #df.set_index('gene', inplace=True)
 
-    # non_zero_genes = df[df['DEG'] != 0].index
-    # df = df[df.index.isin(non_zero_genes)]
-    #gene2value = df[['-log10(fdr)','log2(old/young)', 'DEG']]
-    gene2value = df[['DEG']]
+DMR_FEATURE_NAMES = ['2mo', '9mo', '18mo', '9mo-2mo', '18mo-9mo', '18mo-2mo', 'log2(gene_length)', 'log2(r_length)', 'log2(r_length/gene_length)', 'log2(distance)']
+CG_GENEBODY_FEATURE_NAMES = ['2mo', '9mo', '18mo', '9mo-2mo', '18mo-9mo', '18mo-2mo', 'log2(gene_length)','(9mo-2mo)*log2(gene_length)',  '(18mo-2mo)*log2(gene_length)', 
+                            '(18mo-9mo)*log2(gene_length)','DMG','pvalue']
+CH_GENEBODY_FEATURE_NAMES = ['2mo', '9mo', '18mo', '9mo-2mo', '18mo-9mo', '18mo-2mo', 'log2(gene_length)','(9mo-2mo)*log2(gene_length)',  '(18mo-2mo)*log2(gene_length)', 
+                            '(18mo-9mo)*log2(gene_length)','DMG','pvalue']
+ATAC_FEATURE_NAMES = ['2mo', '9mo', '18mo', 'log2(9mo/2mo)', 'log2(18mo/9mo)', 'log2(18mo/2mo)', 'log2(gene_length)', 'log2(distance)','DAR']
+HIC_FEATURE_NAMES = [ 'Tanova', '2mo.Q', '9mo.Q', '18mo.Q','9mo-2mo.Q','18mo-9mo.Q', '18mo-2mo.Q',
+                        'log2(gene_length)', 'log2(anchor1_distance)','log2(anchor2_distance)','Diff_Loop'] #'Qanova', 'Eanova',,'2mo.T', '9mo.T', '18mo.T','9mo-2mo.T', '18mo-9mo.T', '18mo-2mo.T', 
+ABC_DMR_NAMES = ['2mo.activity', '2mo.contact', '2mo.abc_score', '9mo.activity','9mo.contact', '9mo.abc_score', 
+                    '18mo.activity', '18mo.contact','18mo.abc_score', 'log2(eg_distance)','log2(gene_length)','log2(contact_distance)']
+ABC_peak_NAMES = ['2mo.activity', '2mo.contact', '2mo.abc_score', '9mo.activity','9mo.contact', '9mo.abc_score', 
+                    '18mo.activity', '18mo.contact','18mo.abc_score', 'log2(eg_distance)','log2(gene_length)','log2(contact_distance)']
 
-    # df = df[df.index.isin(non_zero_genes)]
-    # use all pleak/loop, adding columns as pvalue, anova, 
-    DMR_FEATURE_NAMES = ['2mo', '9mo', '18mo', '9mo-2mo', '18mo-9mo', '18mo-2mo', 'log2(gene_length)', 'log2(r_length)', 'log2(r_length/gene_length)', 'log2(distance)']
-    CG_GENEBODY_FEATURE_NAMES = ['2mo', '9mo', '18mo', '9mo-2mo', '18mo-9mo', '18mo-2mo', 'log2(gene_length)','(9mo-2mo)*log2(gene_length)',  '(18mo-2mo)*log2(gene_length)', 
-                                '(18mo-9mo)*log2(gene_length)','DMG','pvalue']
-    CH_GENEBODY_FEATURE_NAMES = ['2mo', '9mo', '18mo', '9mo-2mo', '18mo-9mo', '18mo-2mo', 'log2(gene_length)','(9mo-2mo)*log2(gene_length)',  '(18mo-2mo)*log2(gene_length)', 
-                                '(18mo-9mo)*log2(gene_length)','DMG','pvalue']
-    ATAC_FEATURE_NAMES = ['2mo', '9mo', '18mo', 'log2(9mo/2mo)', 'log2(18mo/9mo)', 'log2(18mo/2mo)', 'log2(gene_length)', 'log2(distance)','DAR']
-    HIC_FEATURE_NAMES = [ 'Tanova', '2mo.Q', '9mo.Q', '18mo.Q','9mo-2mo.Q','18mo-9mo.Q', '18mo-2mo.Q',
-                         'log2(gene_length)', 'log2(anchor1_distance)','log2(anchor2_distance)','Diff_Loop'] #'Qanova', 'Eanova',,'2mo.T', '9mo.T', '18mo.T','9mo-2mo.T', '18mo-9mo.T', '18mo-2mo.T', 
-    ABC_DMR_NAMES = ['2mo.activity', '2mo.contact', '2mo.abc_score', '9mo.activity','9mo.contact', '9mo.abc_score', 
-                     '18mo.activity', '18mo.contact','18mo.abc_score', 'log2(eg_distance)','log2(gene_length)','log2(contact_distance)']
-    ABC_peak_NAMES = ['2mo.activity', '2mo.contact', '2mo.abc_score', '9mo.activity','9mo.contact', '9mo.abc_score', 
-                     '18mo.activity', '18mo.contact','18mo.abc_score', 'log2(eg_distance)','log2(gene_length)','log2(contact_distance)']
 
-    
-    
-    DATA_FEATURE_NAMES = {
-        'dmr': DMR_FEATURE_NAMES,
-        'mcg_genebody': CG_GENEBODY_FEATURE_NAMES,
-        'mch_genebody': CH_GENEBODY_FEATURE_NAMES,
-        'atac': ATAC_FEATURE_NAMES,
-        'hic_loop': HIC_FEATURE_NAMES,
-        'hic_abc_dmr':ABC_DMR_NAMES,
-        'hic_abc_peak':ABC_peak_NAMES 
-    }
 
-    DATA = {}
+DATA_FEATURE_NAMES = {
+    'dmr': DMR_FEATURE_NAMES,
+    'mcg_genebody': CG_GENEBODY_FEATURE_NAMES,
+    # 'mch_genebody': CH_GENEBODY_FEATURE_NAMES,
+    'atac': ATAC_FEATURE_NAMES,
+    'hic_loop': HIC_FEATURE_NAMES,
+    # 'hic_abc_dmr':ABC_DMR_NAMES,
+    # 'hic_abc_peak':ABC_peak_NAMES 
+}
 
+DATA_FEATURE_NAMES_LIST = list(DATA_FEATURE_NAMES.keys())
+
+
+
+def get_dmr_feat(ct):
     dmr = pd.read_csv(f'ml_input/{ct}/{ct}.aDMR_gene.csv')
     dmr_feat = dmr
     dmr_feat.rename(columns={'gene_name': 'gene'}, inplace=True)
@@ -57,9 +52,10 @@ def load_data(ct):
     dmr_feat = dmr_feat[['gene', *DMR_FEATURE_NAMES]]
     assert dmr_feat.isna().sum().sum() == 0
     assert dmr_feat.isin([np.inf, -np.inf]).sum().sum() == 0
-    DATA['dmr'] = dmr_feat
     print('Processed dmr data')
+    return dmr_feat
 
+def get_genebody_feat(ct):
     mcg_genebody = pd.read_csv(f'ml_input/{ct}/{ct}.mCG_genebody_gene.csv')
     mcg_genebody_feat = mcg_genebody
     mcg_genebody_feat.rename(columns={'gene_name': 'gene'}, inplace=True)
@@ -74,9 +70,11 @@ def load_data(ct):
     mcg_genebody_feat= mcg_genebody_feat.dropna()
     assert mcg_genebody_feat.isna().sum().sum() == 0
     assert mcg_genebody_feat.isin([np.inf, -np.inf]).sum().sum() == 0
-    DATA['mcg_genebody'] = mcg_genebody_feat
     print('Processed mCG genebody data')
-    
+    return mcg_genebody_feat
+
+
+def get_mch_genebody_feat(ct):
     mch_genebody = pd.read_csv(f'ml_input/{ct}/{ct}.mCH_genebody_gene.csv')
     mch_genebody_feat = mch_genebody
     mch_genebody_feat.rename(columns={'gene_name': 'gene'}, inplace=True)
@@ -91,10 +89,11 @@ def load_data(ct):
     mch_genebody_feat= mch_genebody_feat.dropna()
     assert mch_genebody_feat.isna().sum().sum() == 0
     assert mch_genebody_feat.isin([np.inf, -np.inf]).sum().sum() == 0
-    DATA['mch_genebody'] = mch_genebody_feat
     print('Processed mCH genebody data')
+    return mch_genebody_feat
 
 
+def get_atac_feat(ct):
     atac = pd.read_csv(f'ml_input/{ct}/{ct}.peak_gene.csv')
     atac_feat = atac
     atac_feat.rename(columns={'gene_name': 'gene'}, inplace=True)
@@ -107,9 +106,10 @@ def load_data(ct):
     #check if any na or inf 
     assert atac_feat.isna().sum().sum() == 0
     assert atac_feat.isin([np.inf, -np.inf]).sum().sum() == 0
-    DATA['atac'] = atac_feat
     print('Processed atac data')
+    return atac_feat
 
+def get_hic_feat(ct):
     hic = pd.read_csv(f'ml_input/{ct}/{ct}.Loop_gene.csv.gz')
     hic_feat = hic
     hic_feat.rename(columns={'gene_name': 'gene'}, inplace=True)
@@ -126,9 +126,11 @@ def load_data(ct):
     hic_feat = hic_feat[['gene', *HIC_FEATURE_NAMES]]
     assert hic_feat.isna().sum().sum() == 0
     assert hic_feat.isin([np.inf, -np.inf]).sum().sum() == 0
-    DATA['hic_loop'] = hic_feat
     print('Processed hic loop data')
+    return hic_feat
 
+
+def get_abc_dmr_feat(ct):
     abc_dmr = pd.read_csv(f'ml_input/{ct}/{ct}.abc_enhancer.DMR_gene.csv').fillna(0)
     abc_dmr_feat = abc_dmr
     abc_dmr_feat.rename(columns={'gene_name': 'gene'}, inplace=True)
@@ -138,10 +140,12 @@ def load_data(ct):
     abc_dmr_feat = abc_dmr_feat[['gene', *ABC_DMR_NAMES]]
     assert abc_dmr_feat.isna().sum().sum() == 0
     assert abc_dmr_feat.isin([np.inf, -np.inf]).sum().sum() == 0
-    DATA['hic_abc_dmr'] = abc_dmr_feat
-    print('Processed hic_abc_dmr data')
+    print('Processed abc dmr data')
+    return abc_dmr_feat
 
 
+
+def get_abc_peak_feat(ct):
     abc_peak =  pd.read_csv(f'ml_input/{ct}/{ct}.abc_enhancer.peak_gene.csv').fillna(0)
     abc_peak_feat = abc_peak
     abc_peak_feat.rename(columns={'gene_name': 'gene'}, inplace=True)
@@ -151,9 +155,49 @@ def load_data(ct):
     abc_peak_feat = abc_peak_feat[['gene', *ABC_peak_NAMES]]
     assert abc_peak_feat.isna().sum().sum() == 0
     assert abc_peak_feat.isin([np.inf, -np.inf]).sum().sum() == 0
-    DATA['hic_abc_peak'] = abc_peak_feat
-    print('Processed hic_abc_peak data')
+    print('Processed abc peak data')
+    return abc_peak_feat
 
+def load_data(ct):
+    df = pd.read_csv(f'ml_input/{ct}/{ct}.luisa_RNA_DEG.csv', index_col =0)
+    #df.set_index('gene', inplace=True)
+
+    # non_zero_genes = df[df['DEG'] != 0].index
+    # df = df[df.index.isin(non_zero_genes)]
+    #gene2value = df[['-log10(fdr)','log2(old/young)', 'DEG']]
+    gene2value = df[['DEG']]
+
+    # df = df[df.index.isin(non_zero_genes)]
+    # use all pleak/loop, adding columns as pvalue, anova, 
+ 
+    DATA = {}
+
+    FEATURE_LOADING_FUNCTIONS = {
+        'dmr': get_dmr_feat,
+        'mcg_genebody': get_genebody_feat,
+        'mch_genebody': get_mch_genebody_feat,
+        'atac': get_atac_feat,
+        'hic_loop': get_hic_feat,
+        'hic_abc_dmr': get_abc_dmr_feat,
+        'hic_abc_peak': get_abc_peak_feat
+    }
+    
+    # Define tasks to run in parallel
+    tasks = [
+        (name, FEATURE_LOADING_FUNCTIONS[name]) for name in DATA_FEATURE_NAMES_LIST
+    ]
+
+    # Run tasks in parallel using ThreadPoolExecutor
+    with ThreadPoolExecutor() as executor:
+        # Submit all tasks and store futures
+        futures = {
+            name: executor.submit(func, ct) 
+            for name, func in tasks
+        }
+        
+        # Get results as they complete and store in DATA
+        for name, future in futures.items():
+            DATA[name] = future.result()
 
     index_order = gene2value.index.tolist()
     # Train a sequence model on dmr_feat to predict gene2value['log2(old/young)']
@@ -164,11 +208,23 @@ def load_data(ct):
 
     X = {}
     # Step 1: Prepare the data
-    for feature_type, features in DATA.items():
+    print('Preparing data')
+    def prepare_data(feature_type):
+        features = DATA[feature_type]
         feature_names = DATA_FEATURE_NAMES[feature_type]
         list_feat = features.groupby('gene').apply(lambda x: x[feature_names].values.tolist())
         list_feat = list_feat.reindex(index_order, fill_value=[[0] * len(feature_names)])
-        X[feature_type] = list_feat.values.tolist()
+        return list_feat.values.tolist()
+    with ThreadPoolExecutor() as executor:
+        # Submit all tasks and store futures
+        futures = {
+            feature_type: executor.submit(prepare_data, feature_type) 
+            for feature_type in DATA
+        }
+        
+        # Get results as they complete and store in DATA
+        for feature_type, future in futures.items():
+            X[feature_type] = future.result()
 
     # y = gene2value['DEG'].values.tolist()
     # y = np.array([int(i) for i in y])
@@ -199,8 +255,21 @@ def get_balanced_data(data):
 
     # Create balanced dataset
     X_balanced = {}
-    for feature_type, features in data['X'].items():
-        X_balanced[feature_type] = [features[i] for i in sampled_indices]
+
+    print('Getting balanced data')
+    def index_features(feature_type):
+        features = data['X'][feature_type]
+        return [features[i] for i in sampled_indices]
+
+    with ThreadPoolExecutor() as executor:
+        # Submit all tasks and store futures
+        futures = {
+            feature_type: executor.submit(index_features, feature_type) 
+            for feature_type in data['X']
+        }
+        # Get results as they complete and store in X_balanced
+        for feature_type, future in futures.items():
+            X_balanced[feature_type] = future.result()
     y_balanced = data['y'][sampled_indices, :]
     return X_balanced, y_balanced
 
